0828e247f1761c420018870ea26b39b968f157f0
Julian Akkaya <julian.akkaya@axelspringer.com>
2021-12-01 09:41:03 +0100

    Solved day one both parts

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Long>): Long {
+        var increaseCounter = 0L
+        var depth: Long? = null
+
+        input.forEach {
+            if (depth != null && depth!! < it) {
+                increaseCounter++
+            }
+            depth = it
+        }
+
+        return increaseCounter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Long>): Long {
+        val inputSums: MutableList<Long> = mutableListOf()
+        val until1 = input.size - 2
+        for (i in 0 until until1) {
+            inputSums.add((input[i] + input[i + 1] + input[i + 2]))
+        }
+
+        return part1(inputSums)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInputPart1 = readInput("Day01_01_test").map { it.toLong() }
+    val testInputPart2 = readInput("Day01_02_test").map { it.toLong() }
+    check(part1(testInputPart1) == 7L)
+    check(part2(testInputPart2) == 5L)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toLong() }
     println(part1(input))
     println(part2(input))
 }
