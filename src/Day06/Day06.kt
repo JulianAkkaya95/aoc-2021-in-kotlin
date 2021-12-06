e6b0b433dab7fb87db791791378223e2606267f2
Julian Akkaya <julian.akkaya@axelspringer.com>
2021-12-06 21:29:59 +0100

    Day 6 part2

--- src/Day06/Day06.kt
+++ src/Day06/Day06.kt
@@ -7,8 +7,8 @@ import kotlin.streams.toList
 fun main() {
     fun part1(lanternFishes: MutableList<LanternFish>, days: Int): Long {
         var newLanternFishes: MutableList<LanternFish> = mutableListOf()
-        for (i in 1..days) {
-            lanternFishes.forEach{ fish ->
+        repeat(days) {
+            lanternFishes.forEach { fish ->
                 fish.newDay().ifPresent { newFish -> newLanternFishes.add(newFish) }
             }
             lanternFishes.addAll(newLanternFishes)
@@ -17,13 +17,42 @@ fun main() {
         return lanternFishes.size.toLong()
     }
 
+    fun part2(input: Array<Long>, days: Int): Long {
+        var fishes = input
 
-    val testInput = readInput("Day06/Day06_test")[0].split(",").stream().map { it.toInt() }.map { LanternFish(it) }.toList().toMutableList()
-    check(part1(testInput.toMutableList(), 80) == 5934L)
-    //check(part1(testInput.toMutableList(), 256) == 26984457539)
+        repeat(days) {
+            val newFishes = Array(9) { 0L }
+            for (i in 0..8) {
+                if (i == 0) {
+                    newFishes[6] += fishes[i]
+                    newFishes[8] += fishes[i]
+                } else {
+                    newFishes[i - 1] += fishes[i]
+                }
+            }
+            fishes = newFishes
+        }
 
+        return fishes.sum()
+    }
+    
+    val testInput = readInput("Day06/Day06_test")[0].split(",").stream().map { it.toInt() }.toList()
+    val testInputPart1 = testInput.map { LanternFish(it) }.toList()
+    val testInputPart2 = Array(9) { 0L }
+    testInput.forEach {
+        testInputPart2[it]++
+    }
 
-    //val input = readInput("Day06/Day06")[0].split(",").stream().map { it.toInt() }.map { LanternFish(it) }.toList().toMutableList()
-    //println(part1(input.toMutableList(), 80))
-    //println(part1(input.toMutableList(), 256))
+    check(part1(testInputPart1.toMutableList(), 80) == 5934L)
+    check(part2(testInputPart2, 256) == 26984457539)
+
+    val input = readInput("Day06/Day06")[0].split(",").stream().map { it.toInt() }.toList()
+    val inputPart2 = Array(9) { 0L }
+    val inputPart1 = input.map { LanternFish(it) }.toList().toMutableList()
+    input.forEach {
+        inputPart2[it]++
+    }
+
+    println(part1(inputPart1, 80))
+    println(part2(inputPart2, 256))
 }
