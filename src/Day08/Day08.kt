f5f29236f822ed9268f7b867194cf1e21b1fb16b
Julian Akkaya <julian.akkaya@axelspringer.com>
2021-12-08 16:15:36 +0100

    Add day 8 part 1

---
+++ src/Day08/Day08.kt
@@ -0,0 +1,34 @@
+package Day08
+
+import readInput
+import kotlin.streams.toList
+
+fun main() {
+    val digits = mapOf(Pair("one", 2), Pair("four", 4), Pair("eight", 7), Pair("seven", 3))
+
+    fun part1(input: List<String>): Long {
+
+        println(input)
+        var counter = 0L;
+        input.forEach {
+            if (digits.containsValue(it.length)) {
+                counter++
+                println(it)
+            }
+
+        }
+
+        return counter
+    }
+
+
+    val inputTest = readInput("Day08/Day08_test").stream().map { it.split(" | ").last() }.toList().joinToString(" ").split(" ")
+
+    check(part1(inputTest) == 26L)
+    //check(part2(horizontalTest) == 168L)
+
+    val input = readInput("Day08/Day08").stream().map { it.split(" | ").last() }.toList().joinToString(" ").split(" ")
+
+    println(part1(input))
+    //println(part2(horizontal))
+}
